c3c8628ce2edd00777e246d13c1e76bb0546ec45
Mounir <lm_melzi@esi.dz>
2023-03-26 06:48:22 +0200

    add responsive admin's login page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -16,8 +16,8 @@ function Admin() {
   const router = useRouter();
 
   async function sumbitHandler(e) {
-    setLoading(true);
     e.preventDefault();
+    setLoading(true);
 
     const username = usernameInputRef.current.value;
     const password = passwordInputRef.current.value;
@@ -40,22 +40,27 @@ function Admin() {
       <Head>
         <title>Login</title>
       </Head>
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center h-screen bg-gray-100">
+
+      <main
+        style={{ backgroundImage: `url(/assets/images/bg_login_admin.jpg)` }}
+        className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center h-screen bg-gray-100"
+      >
         <form
-          className="bg-white rounded-2xl shadow-2xl w-2/5 max-w-4xl py-14 px-16 relative"
+          className=" sm:bg-white sm:rounded-2xl sm:shadow-2xl w-[350px] sm:w-[700px] sm:max-w-5xl sm:min-w-max sm:py-14 sm:px-16 relative"
           onSubmit={sumbitHandler}
         >
-          <div className="absolute -top-10 left-[45%] shadow-2xl">
+          <div className="absolute -top-20 left-[40%] sm:-top-10 sm:left-[45%] shadow-2xl">
             <Image
               src="/assets/logos/logo.png"
               alt="amaBank logo"
-              width={70}
-              height={70}
+              width={400}
+              height={400}
+              className="w-[70px] h-[71px]"
             />
           </div>
 
           <div className="px-20 container flex justify-center">
-            <h2 className="text-xl font-semibold mb-2 ">
+            <h2 className="sm:text-2xl font-semibold mb-2 ">
               Connexion administrateur
             </h2>
           </div>
@@ -81,7 +86,6 @@ function Admin() {
             </div>
             <div className="flex flex-col items-center mb-3">
               <div className="bg-gray-100 w-11/12 p-4 rounded-sm border flex items-center">
-                {" "}
                 <BiLock size={25} />
                 <input
                   type="password"
@@ -95,7 +99,7 @@ function Admin() {
 
             <div className="flex flex-col items-center mt-9">
               <button
-                className="rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-green-500 flex items-center"
+                className="rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-green-600 disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
                 type="submit"
                 disabled={loading}
               >
@@ -122,7 +126,7 @@ export async function getServerSideProps(context) {
   if (session) {
     return {
       redirect: {
-        destination: "/",
+        destination: process.env.NEXT_PUBLIC_APP_URL + "/admin/home",
         permanent: false,
       },
     };

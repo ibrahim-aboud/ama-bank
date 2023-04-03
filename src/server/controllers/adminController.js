00419486ad763d796c6676514ba393388c2f3bc6
Mounir <lm_melzi@esi.dz>
2023-04-04 01:56:38 +0200

    add backend logic for admin

--- src/server/controllers/adminController.js
+++ src/server/controllers/adminController.js
@@ -1,37 +1,87 @@
-import dbQuery from "@/server/db/adminDbConnect";
 import userCredsValidator from "@/lib/validations/userCredsValidator";
+import adminInfoValidator from "@/lib/validations/adminInfoValidator";
+import Admin from "../models/adminModel";
+import hashPassword from "@/lib/utils/hashPassword";
 
 export default class AdminController {
   async loginAdmin(username, password) {
     // validate user inputs
     const check = userCredsValidator(username, password);
-
     if (check.error) {
       throw new Error(check.errorList[0]);
     }
 
-    // search for the admin in db
-    var result = null;
-
-    try {
-      result = await dbQuery("SELECT * FROM ab_admins WHERE admin_name=(?)", [
-        username,
-      ]);
-    } catch (e) {
-      throw new Error("Something went wrong!");
-    }
-
-    if (!result || result.length == 0) {
-      throw new Error("L'utilisateur n'existe pas");
-    }
-
-    const user = result[0];
+    const user = await Admin.getAdminByUsername(username);
 
     // check if the password is correct
-    if (user.admin_password !== password) {
+    if (user.password !== hashPassword(password)) {
       throw new Error("Le mot de passe est incorrect. essayez à nouveau");
     }
 
+    delete user.password;
     return user;
+  }
+
+  async get(req, res) {
+    const { id } = req.query;
+
+    if (isNaN(id)) {
+      res.status(404).send("L'admin n'existe pas!");
+      return;
+    }
+
+    try {
+      const admin = await Admin.getAdminById(id);
+      delete admin.password; // don't send the password to the client
+
+      res.status(200).json({ admin });
+      return;
+    } catch (e) {
+      res.status(e.status || 500).send(e.message);
+      return;
+    }
+  }
+
+  async put(req, res) {
+    const { admin: data, oldPassword } = req.body;
+
+    if (!data) {
+      res.status(409).send("No admin to update");
+      return;
+    }
+
+    const admin = new Admin(data.id, data.username, data.email, data.password);
+
+    if (!admin) {
+      res.status(409).send("No admin to update");
+      return;
+    }
+
+    const check = adminInfoValidator(admin);
+    if (check.error) {
+      res.status(400).send(check.errorList[0]);
+      return;
+    }
+
+    const oldAdmin = await Admin.getAdminById(admin.id);
+    if (oldAdmin.password !== hashPassword(oldPassword)) {
+      res.status(401).send("Le mot de passe est incorrect");
+      return;
+    }
+
+    // hash the password before updating the admin
+    admin.password = hashPassword(admin.password);
+
+    try {
+      const data = await Admin.updateAdmin(admin);
+      delete data.old.password;
+      delete data.new.password;
+
+      res.status(201).json({ admin: data });
+      return;
+    } catch (e) {
+      res.status(e.status || 500).send(e.message);
+      return;
+    }
   }
 }

2cbfd49efacf090ad650ac8fa65167c2a4c33401
Raouf Medjadj <lm_medjadj@esi.dz>
2023-04-17 19:53:09 +0100

    added responsiveness (V-alpha)

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,25 +23,33 @@ function BankListElement(props) {
 
   return (
     <div>
-      <div className="flex justify-center items-center mb-10">
-        <Image
-          src={props.logo_src}
-          alt={`${props.name} logo`}
-          width={400}
-          height={400}
-          className="w-[80px] h-[80px] shadow-lg"
-        />
-        <h2 className="font-bold pl-8 text-2xl w-[35%]">{props.name}</h2>
+      <div className="flex justify-center items-center mb-5 md:mb-10">
+        <div className="flex flex-col sm:flex-row sm:items-center w-[150px] sm:w-[400px]">
+          <Image
+            src={props.logo_src}
+            alt={`${props.name} logo`}
+            width={400}
+            height={400}
+            className="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] md:w-[80px] md:h-[80px] shadow-lg"
+          />
+          <h2 className="font-bold sm:pl-[5%] sm:text-md smx:mt-2 lg:text-2xl">{props.name}</h2>
+        </div>
 
-        <button className="rounded-xl px-5 py-3 font-semibold bg-[#40916C] mr-2 text-white shadow-md hover:bg-[#51b186] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300">
-          Comparer avec une autre banque
-          <MdOutlineCompareArrows size={23} className="ml-2" />
-        </button>
+        <div className="flex smx:flex-col items-center">
+          <button className="rounded-xl px-5 smx:mb-1 smx:py-2 py-3 font-semibold bg-[#40916C] lg:mr-2 text-white shadow-md hover:bg-[#51b186] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300">
+            <h2 className="smx:hidden">Comparer avec une autre banque</h2>
+            <h2 className="lg:hidden text-[0.7rem]">Comparer</h2>
+            <MdOutlineCompareArrows size={23} className="ml-2 smx:hidden" />
+            <MdOutlineCompareArrows size={17} className="ml-2 lg:hidden" />
+          </button>
 
-        <button className="rounded-xl px-5 py-3 font-semibold bg-[#40916C] text-white shadow-md hover:bg-[#51b186] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300">
-          Plus de details
-          <TbListDetails size={23} className="ml-2" />
-        </button>
+          <button className="rounded-xl px-5 smx:py-2 py-3 font-semibold bg-[#40916C] text-white shadow-md hover:bg-[#51b186] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300">
+            <h2 className="smx:hidden">Plus de details</h2>
+            <h2 className="lg:hidden text-[0.7rem]">Details</h2>
+            <TbListDetails size={23} className="ml-2 smx:hidden" />
+            <TbListDetails size={17} className="ml-2 lg:hidden" />
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -205,16 +213,16 @@ export default function Home({ banks }) {
 
   return (
     <div>
-      <div className="h-screen mt-1">
+      <div className="mt-1">
         <Slideshow />
       </div>
 
       <div>
-        <div className="flex flex-col items-center mt-14 h-screen">
-          <div className="flex items-center mb-16">
-            <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
-              <h2 className="p-1 text-lg">Nom de la banque</h2>
-              <div className="px-4 py-2 rounded-md border shadow-sm flex items-center w-full bg-gray-100">
+        <div className="flex flex-col items-center my-14 py-[1%] px-[10%]">
+          <div className="flex items-center mb-5 md:mb-16">
+            <div className=" mb-4 xs:w-[300px] sm:w-[500px] lg:w-[800px] lgx:w-[1000px]">
+              <h2 className="p-1 text-[1rem]">Nom de la banque</h2>
+              <div className="px-3 md:px-7 py-1 sm:px-4 sm:py-2 rounded-md border shadow-sm flex items-center w-full bg-gray-100">
                 <div className="w-full">
                   <input
                     type="text"
@@ -230,20 +238,20 @@ export default function Home({ banks }) {
             </div>
             <div>
               <button
-                className="rounded-xl px-10 py-4 mt-4 ml-2 font-semibold bg-black text-white hover:bg-[#40916C] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
+                className="rounded-xl p-3 lg:px-8 sm:p-4 mt-4 ml-2 font-semibold bg-gray-100 border border-gray-300 text-gray-600 hover:text-white hover:bg-[#40916C] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
                 onClick={() => {
                   setIsVisible(!isVisible);
                 }}
               >
-                Filtrer
-                <BiFilterAlt size={23} className="ml-2" />
+                <h2 className="smx:hidden">Filtrer</h2>
+                <BiFilterAlt size={23} className="lg:ml-2" />
               </button>
             </div>
           </div>
 
-          <div className="w-full px-[20%] h-[540px]">
+          <div className="w-[340px] xs:w-[400px] sm:w-[600px] md:w-[700px] lg:w-[1000px] lgx:w-[1200px] h-[540px]">
             <Scrollbar>
-              <div className="w-full bg-[#d9d9d928] py-16 rounded-md">
+              <div className="w-full bg-[#d9d9d928] py-10 md:py-16 rounded-md">
                 {filteredList.map((bank) => (
                   <BankListElement
                     key={bank.id}

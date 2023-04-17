9abc4f9df2069d41bbc62eb889b308b79b88eec4
Oualid CHABANE <chbwalid7@gmail.com>
2023-04-17 21:30:16 +0200

    compareToListeOfPrestationsV2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,5 @@
 import Head from "next/head";
-/* import ComparaisonListe from "../components/compareTo/ComparaisonListe.js"
-import DeletePopUp from "../components/editBankDataPage/deleteConfirmation.js"
-import SuccesPopUp from "../components/editBankDataPage/success.js"
-import ModficationListe from "../components/editBankDataPage/modificationList.js"
-import AgencyListe from "../components/editBankDataPage/agencyListe.js"
-import Header from  "../components/editBankDataPage/headerListeOfModification.js"
-import SearchBars from "../components/editBankDataPage/searchBars.js"
-import EditBankData from "src/components/editBankDataPage/editBankData.js" */
-import EditBankDataClient from "src/components/editBankDataPageClient/editBankData.js"
+ import ComparaisonListe from "../components/compareTo/ComparaisonListe.js"
 
 export default function Home() {
   return (
@@ -24,17 +16,7 @@ export default function Home() {
       </Head>
 
       <main>
-     {/*    <ComparaisonListe /> */}
-   {/*      <DeletePopUp />  */} 
-
-{/*          <SuccesPopUp />  */}
-     {/* <ModficationListe record={{message1 : "Ajouter une agence", message2 : "annuler", icone="public/assets/modificationsPage/modificationListeCheck.svg"}} /> */}
-    
-{/*         <Header />
-        <SearchBars />
-        <AgencyListe /> */}
-       {/*   <EditBankData />  */}
-        <EditBankDataClient />
+        <ComparaisonListe />
       </main>
     </> 
     

91f063d8c72f706213e4f88fff876cb388ac3e76
F1OOw <ls_djerrai@esi.dz>
2023-04-19 18:58:01 +0100

    uploadImage in bank Controller

--- src/server/controllers/banks/bankController.js
+++ src/server/controllers/banks/bankController.js
@@ -3,6 +3,9 @@ import Bank from "../../models/bankModel";
 import FilesHelpers from "@/lib/utils/FilesHelpers";
 import { errorMessages } from "@/lib/utils/errorMessages";
 import ModelError from "@/lib/utils/ModelError";
+import banks_logos_folder from "@/lib/utils/globals";
+import banks_images_folder from "@/lib/utils/globals"
+
 
 export default class BankController {
   async get(req,res){
@@ -25,13 +28,13 @@ export default class BankController {
   async uploadLogo(req, res) {
     const storage = multer.diskStorage({
       destination: function (req, file, cb) {
-        cb(null, "./public/assets/logos/banks_logos");
+        cb(null, banks_logos_folder);
       },
       filename: function (req, file, cb) {
         // delete the current logo
         FilesHelpers.deleteFilesInDirectory_IgnoreExtension(
           file.originalname,
-          "./public/assets/logos/banks_logos"
+          banks_logos_folder
         );
 
         cb(null, file.originalname);
@@ -44,7 +47,7 @@ export default class BankController {
     try {
       uploadFile(req, res, (err) => {
         if (err) {
-          throw new Error(err.message);
+          throw new ModelError(err.message,500);
         }
       });
 
@@ -56,4 +59,40 @@ export default class BankController {
       return;
     }
   }
+
+  async uploadImage(req, res) {
+    const storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, banks_images_folder);
+      },
+      filename: function (req, file, cb) {
+        // delete the current logo
+        FilesHelpers.deleteFilesInDirectory_IgnoreExtension(
+          file.originalname,
+          banks_images_folder
+        );
+
+        cb(null, file.originalname);
+      },
+    });
+
+    const upload = multer({ storage: storage });
+    const uploadFile = upload.single("file");
+
+    try {
+      uploadFile(req, res, (err) => {
+        if (err) {
+          throw new ModelError(err.message,500);
+        }
+      });
+
+      // File uploaded successfully
+      res.status(201).send("ok");
+      return;
+    } catch (e) {
+      res.status(500).json({error: new ModelError(errorMessages.serverError,500)}) ;
+      return;
+    }
+  }
+
 }

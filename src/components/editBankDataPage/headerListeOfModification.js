e0d187ae95dda40ebf98f9991368c9a79765698c
Oualid CHABANE <chbwalid7@gmail.com>
2023-04-19 15:29:29 +0200

    fixing warnings

--- src/components/editBankDataPage/headerListeOfModification.js
+++ src/components/editBankDataPage/headerListeOfModification.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Image from "next/Image"
+import Image from "next/image"
 import iconeInd from "public/assets/modificationsPage/indicationIcon.png"
 import styles from "src/styles/agenciesModificaitonStyles/headerListeOfModificaiton.module.css"
 

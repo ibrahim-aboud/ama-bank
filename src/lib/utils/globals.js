60511f76e8466de81edfa77ce7695fa425e4d7d3
F1OOw <ls_djerrai@esi.dz>
2023-04-20 09:23:33 +0100

    adding the global variables

--- src/lib/utils/globals.js
+++ src/lib/utils/globals.js
@@ -5,5 +5,7 @@ export const globals = {
         dg : ["bank_id" ,"address" ,"lat" ,"lng" ,"wilaya" ,"phone" ,"fax" ,"location_link"]
     } ,
     possibleOperations : ["Gestion et tenue de compte","Opération de paiement","Monétique"],
+    banks_logos_folder : "./public/assets/logos/banks_logos" ,
+    banks_images_folder : "./public/assets/images/banks_images"
 }
 

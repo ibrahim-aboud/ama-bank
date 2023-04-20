ef41fcf33a22cc266da508afc643f332ddec16ac
Raouf Medjadj <lm_medjadj@esi.dz>
2023-04-20 18:59:45 +0100

    added dg-backend

--- src/components/admin/banks/general/bankInfoForm+.js
+++ src/components/admin/banks/general/bankInfoForm+.js
@@ -5,8 +5,12 @@ import Image from "next/image";
 import { FiGlobe, FiUpload } from "react-icons/fi";
 import { FaUndo } from "react-icons/fa"
 import { AiOutlineInfoCircle } from "react-icons/ai";
-import { MdCancel } from "react-icons/md";
-import { HiCheckCircle } from "react-icons/hi";
+import { MdFax, MdCancel } from "react-icons/md";
+import { HiCheckCircle, HiPhone, HiLocationMarker, HiSearch } from "react-icons/hi";
+import { RiBankFill } from "react-icons/ri";
+import { wilayas } from "@/lib/utils/wilayaMap";
+import Link from "next/link";
+
 
 function BankInfoFormADD() {
   const [loading, setLoading] = useState(false);
@@ -15,13 +19,23 @@ function BankInfoFormADD() {
   const [selectedFile2, setSelectedFile2] = useState();
 
   const router = useRouter();
-  const logoInputRef = useRef();
 
   const [bank, setBank] = useState({
     name: "",
     description: "",
     websiteLink: "",
   });
+
+  const [dg, setDg] = useState({
+    bank_id: "",
+    address: "",
+    lat: 0,
+    lng: 0,
+    wilaya: "",
+    phone: "",
+    fax: "",
+    location_link: ""
+  })
 
   const [preview1, setPreview1] = useState(null);
   const [preview2, setPreview2] = useState(null);
@@ -44,13 +58,13 @@ function BankInfoFormADD() {
 
     try {
       // post request to the API to check user inputs and add to the database
-      const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + "/banks", {bank: bank});
+      const response1 = await axios.post(process.env.NEXT_PUBLIC_API_URL + "/banks", {bank: bank});
         
-      const data = response.data;
+      const data1 = response1.data;
 
       if (selectedFile1) { // logo
         const fileExtension = selectedFile1.name.split(".").pop();
-        const file = new File([selectedFile1], `${data.id}.${fileExtension}`);
+        const file = new File([selectedFile1], `${data1.id}.${fileExtension}`);
 
         const formData = new FormData();
         formData.append("file", file);
@@ -67,24 +81,32 @@ function BankInfoFormADD() {
       }
       if (selectedFile2) { // image
         const fileExtension = selectedFile2.name.split(".").pop();
-        const file = new File([selectedFile2], `${data.id}.${fileExtension}`);
+        const file = new File([selectedFile2], `${data1.id}.${fileExtension}`);
 
-        // const formData = new FormData();
-        // formData.append("file", file);
+        const formData = new FormData();
+        formData.append("file", file);
 
-        // await axios.post(
-        //   process.env.NEXT_PUBLIC_API_URL + "/bank/logo",
-        //   formData,
-        //   {
-        //     headers: {
-        //       "Content-Type": "multipart/form-data",
-        //     },
-        //   }
-        // );
+        await axios.post(
+            process.env.NEXT_PUBLIC_API_URL + "/bank/image",
+            formData,
+            {
+              headers: {
+                "Content-Type": "multipart/form-data",
+              },
+            }
+          );
       }
 
-      setBank(response.data);
-        console.log(bank);
+      const dgToSend = {...dg, bank_id: data1.id};
+      const response2 = await axios.post(process.env.NEXT_PUBLIC_API_URL + "/dgs", {dg: dgToSend});
+        
+      const data2 = response2.data.dg;
+
+      console.log(bank);
+      console.log(data1);
+      console.log(dgToSend);
+      console.log(data2);
+
       // when the data is updated
       setError("");
       //router.reload();
@@ -97,18 +119,18 @@ function BankInfoFormADD() {
 
   return (
     <div className="mb-20 mt-5">
-      <div className="py-8 lg:mx-16 flex items-center justify-center lg:gap-14">
-        <div className="hidden lg:block h-[2px] bg-black w-[25%]" />
-
-        <div className="flex items-center justify-center gap-4 text-lg sm:text-xl md:text-3xl">
-          <AiOutlineInfoCircle className=" font-bold" />
-          <h3 className="font-bold">Informations générales sur la banque</h3>
-        </div>
-
-        <div className="hidden lg:block h-[2px] bg-black w-[25%]" />
-      </div>
 
       <form className="flex flex-col items-center" onSubmit={sumbitHandler}>
+        <div className="py-8 lg:mx-16 flex items-center justify-center lg:gap-14 w-full">
+            <div className="hidden lg:block h-[2px] bg-black w-[25%]" />
+
+            <div className="flex items-center justify-center gap-4 text-lg sm:text-xl md:text-3xl">
+            <AiOutlineInfoCircle className=" font-bold" />
+            <h3 className="font-bold">Informations générales sur la banque</h3>
+            </div>
+
+            <div className="hidden lg:block h-[2px] bg-black w-[25%]" />
+        </div>
         <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
           <label htmlFor="bank_name" className="block p-1">
             Nom de la banque
@@ -216,17 +238,122 @@ function BankInfoFormADD() {
           </div>
         </div>
 
-        {error && (
+        <div className="py-6 smx:border-t-4 md:mx-16 flex items-center justify-center gap-14 w-full">
+          <div className="hidden lg:block h-[2px] bg-black w-[25%]" />
+
+          <div className="flex items-center justify-center gap-4 text-lg sm:text-xl md:text-3xl">
+            <AiOutlineInfoCircle className=" font-bold" />
+            <h3 className="font-bold">
+              Informations sur la Direction Générale
+            </h3>
+          </div>
+
+          <div className="hidden lg:block h-[2px] bg-black w-[24%]" />
+        </div>
+
+        <div className="flex flex-col items-center w-full">
+          <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
+            <label className="p-1" htmlFor="wilaya">Wilaya</label>
+            <div className="bg-gray-100 w-full px-4 py-3 rounded-md border flex items-center">
+              <HiSearch className="text-gray-600" size={24} />
+              <select
+                id="wilaya"
+                name="wilaya"
+                className="bg-gray-100 outline-none block w-full hover:border-gray-500 px-4 py-2 pr-8 rounded-2xl leading-tight focus:outline-none focus:shadow-outline"
+                onChange={(event) =>
+                    setDg({ ...dg, wilaya: parseInt(event.target.value) })
+                }
+              >
+                <option value="">-- Selectionner une Wilaya --</option>
+                {wilayas.map((wilaya) => (
+                  <option key={wilaya.code} value={+wilaya.code}>
+                    {wilaya.code} - {wilaya.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <div className="mx-5 mb-4  w-[90%] lg:w-[900px]">
+            <label className="p-1" htmlFor="dg_address">Adresse du siège social</label>
+            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
+              <RiBankFill className="text-gray-600" size={24} />
+              <input
+                type="text"
+                id="dg_address"
+                name="dg_address"
+                placeholder="Ex: 99 route de Meftah 16310 Alger"
+                className="bg-gray-100 outline-none px-4 flex-1"
+                onChange={(event) =>
+                    setDg({ ...dg, address: event.target.value })
+                }
+              />
+            </div>
+          </div>
+
+          <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
+            <label className="p-1" htmlFor="dg_phone">Numéro de téléphone</label>
+            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
+              <HiPhone className="text-gray-600" size={24} />
+              <input
+                type="text"
+                name="dg_phone"
+                id="dg_phone"
+                placeholder="Ex: +213 21 98 53 99"
+                className="bg-gray-100 outline-none px-4 flex-1"
+                onChange={(event) =>
+                    setDg({ ...dg, phone: event.target.value })
+                }
+              />
+            </div>
+          </div>
+
+          <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
+            <label className="p-1" htmlFor="dg_fax">Fax</label>
+            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
+              <MdFax className="text-gray-600" size={24} />
+              <input
+                type="text"
+                name="dg_fax"
+                id="dg_fax"
+                placeholder="Ex: +213 21 98 53 99"
+                className="bg-gray-100 outline-none px-4 flex-1"
+                onChange={(event) =>
+                    setDg({ ...dg, fax: event.target.value })
+                }
+              />
+            </div>
+          </div>
+
+          <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
+            <label className="p-1" htmlFor="dg_localtion_url">Localisation GPS (Lien vers Google Maps)</label>
+            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
+              <HiLocationMarker className="text-gray-600" size={24} />
+              <input
+                type="text"
+                name="dg_location_url"
+                id="dg_location_url"
+                placeholder="Ex: https://goo.gl/maps/onJ7hBd4oZ1fMpPj9"
+                className="bg-gray-100 outline-none px-4 flex-1"
+                onChange={(event) =>
+                    setDg({ ...dg, location_link: event.target.value })
+                }
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* {error && (
           <div className="text-rose-500 font-bold text-center overflow-hidden mt-2">
             {error}
           </div>
-        )}
+        )} */}
 
         <div className="mt-4 flex flex-col md:flex-row items-center justify-center gap-5 md:gap-10 w-full lg:w-[800px] lg:justify-between">
           <button
             type="submit"
             disabled={loading}
-            className="mb-1 rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-green-600 disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
+            className="mb-1 rounded-xl px-8 py-3 font-semibold bg-black text-white hover:shadow-xl hover:bg-[#40916C] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
           >
             Ajouter la banque
             <HiCheckCircle size={23} className="ml-2" />
@@ -241,18 +368,33 @@ function BankInfoFormADD() {
               setSelectedFile2(null);
               setPreview1("");
               setPreview2("");
-              //logoInputRef.current.value = "";
+              setBank({
+                name: "",
+                description: "",
+                websiteLink: "",
+              });
+            
+              setDg({
+                bank_id: "",
+                address: "",
+                lat: "",
+                lng: "",
+                wilaya: "",
+                phone: "",
+                fax: "",
+                location_link: "",
+              });
             }}
-            className="rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-green-600 disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
+            className="rounded-xl px-8 py-3 font-semibold bg-black text-white hover:shadow-xl hover:bg-[#daa250] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
           >
             Effacer le formulaire
             <FaUndo size={20} className="ml-2" />
           </button>
 
-          <button className="mb-1 rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-[#EA5455] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300">
+          <Link href="/admin/home" className="mb-1 rounded-xl px-8 py-3 font-semibold bg-black text-white hover:shadow-xl hover:bg-[#EA5455] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300">
             Annuler
             <MdCancel size={23} className="ml-2" />
-          </button>
+          </Link>
         </div>
       </form>
     </div>
--- src/pages/admin/banks/add-bank.js
+++ src/pages/admin/banks/add-bank.js
@@ -1,118 +1,15 @@
 import AdminLayout from "@/layouts/adminLayout";
-import { useState } from "react";
-import { wilayas } from "@/lib/utils/wilayaMap";
 import { getSession, useSession } from "next-auth/react";
-import { AiOutlineInfoCircle } from "react-icons/ai";
-import { FiUpload, FiGlobe, FiPhone } from "react-icons/fi";
-import { HiPhone, HiLocationMarker, HiSearch } from "react-icons/hi";
-import { RiBankFill } from "react-icons/ri";
-import { MdFax, MdAddCircle, MdCancel } from "react-icons/md";
-import { FaUndo } from "react-icons/fa";
-import Image from "next/image";
 import BankInfoFormADD from "@/components/admin/banks/general/bankInfoForm+";
 
 function NewBank() {
-  const [selectedWilaya, setSelectedWilaya] = useState(16);
-
-  const handleWilayaChange = (event) => {
-    setSelectedWilaya(event.target.value);
-  };
+  
 
   const session = useSession();
 
   return (
     <div>
       <BankInfoFormADD />
-
-      <div>
-        <div className="py-6 smx:border-t-4 md:mx-16 flex items-center justify-center gap-14">
-          <div className="hidden lg:block h-[2px] bg-black w-[25%]" />
-
-          <div className="flex items-center justify-center gap-4 text-lg sm:text-xl md:text-3xl">
-            <AiOutlineInfoCircle className=" font-bold" />
-            <h3 className="font-bold">
-              Informations sur la Direction Générale
-            </h3>
-          </div>
-
-          <div className="hidden lg:block h-[2px] bg-black w-[24%]" />
-        </div>
-
-        <div className="flex flex-col items-center">
-          <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
-            <h2 className="p-1 ">Wilaya</h2>
-            <div className="bg-gray-100 w-full px-4 py-3 rounded-md border flex items-center">
-              <HiSearch className="text-gray-600" size={24} />
-              <select
-                id="wilaya"
-                name="wilaya"
-                className="bg-gray-100 outline-none block w-full hover:border-gray-500 px-4 py-2 pr-8 rounded-2xl leading-tight focus:outline-none focus:shadow-outline"
-                value={selectedWilaya}
-                onChange={handleWilayaChange}
-              >
-                <option value="">-- Selectionner une Wilaya --</option>
-                {wilayas.map((wilaya) => (
-                  <option key={wilaya.code} value={wilaya.name}>
-                    {wilaya.code} - {wilaya.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-
-          <div className="mx-5 mb-4  w-[90%] lg:w-[900px]">
-            <h2 className="p-1 ">Adresse du siège social</h2>
-            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
-              <RiBankFill className="text-gray-600" size={24} />
-              <input
-                type="text"
-                name="bank_address"
-                placeholder="Ex: 99 route de Meftah 16310 Alger"
-                className="bg-gray-100 outline-none px-4 flex-1"
-              />
-            </div>
-          </div>
-
-          <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
-            <h2 className="p-1 ">Numéro de téléphone</h2>
-            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
-              <HiPhone className="text-gray-600" size={24} />
-              <input
-                type="text"
-                name="bank_phone"
-                placeholder="Ex: +213 21 98 53 99"
-                className="bg-gray-100 outline-none px-4 flex-1"
-              />
-            </div>
-          </div>
-
-          <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
-            <h2 className="p-1 ">Fax</h2>
-            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
-              <MdFax className="text-gray-600" size={24} />
-              <input
-                type="text"
-                name="bank_fax"
-                placeholder="Ex: +213 21 98 53 99"
-                className="bg-gray-100 outline-none px-4 flex-1"
-              />
-            </div>
-          </div>
-
-          <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
-            <h2 className="p-1 ">Localisation GPS (Lien vers Google Maps)</h2>
-            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
-              <HiLocationMarker className="text-gray-600" size={24} />
-              <input
-                type="text"
-                name="bank_location_url"
-                placeholder="Ex: https://goo.gl/maps/onJ7hBd4oZ1fMpPj9"
-                className="bg-gray-100 outline-none px-4 flex-1"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }

508982d6516b56ecbea8924b80e562a751c6d678
ABOUD Ibrahim <li_aboud@esi.dz>
2023-04-22 10:56:26 +0100

    almost done with front end of tarif list

--- src/components/common/ligneCondition.js
+++ src/components/common/ligneCondition.js
@@ -1,11 +1,38 @@
 import style from "@/styles/ligneCondition.module.css";
 
 function LigneCondition({condition1,condition2}){
+    function getPeriod(period){
+        switch(period){
+            case 0 : return "";
+            case 1 : return "/JOUR";
+            case 7 : return "/SEMAINE";
+            case 30 : return "/MOIS";
+            case 90 : return "/TRIMESTRE";
+            case 180 : return "/SEMESTRE";
+            case 360 : return "/AN"
+            default : return "";
+        }
+    }
+    function getTarif(tarif){
+        if (tarif===0){
+            return "GRATUIT";
+        }
+        else if (tarif > 0){
+            return tarif + " DA";
+        }
+        else return "non défini";
+    }
     return (
         <div className="LignCondition">
             <div className={style.container}>
-                <div>{condition1.nom_prestation}{condition1.tarif}{condition1.period}</div>
-                <div>{condition2.nom_prestation}{condition2.tarif}{condition2.period}</div>
+                <div className={style.nomTarifPrestation}>
+                    <div className={style.nomPrestation}>{condition1.nom_prestation}</div>
+                    <div className={style.tarifPrestation}>{getTarif(condition1.tarif) + getPeriod(condition1.period)}</div>
+                </div>
+                <div className={style.nomTarifPrestation}>
+                    <div className={style.nomPrestation}>{condition2.nom_prestation}</div>
+                    <div className={style.tarifPrestation}>{getTarif(condition2.tarif) + getPeriod(condition2.period) }</div>
+                </div>
             </div>
         </div>
     )

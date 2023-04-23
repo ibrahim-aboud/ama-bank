8c310107e690ab8813f02978bfb087cdce2ebe37
ABOUD Ibrahim <li_aboud@esi.dz>
2023-04-23 21:02:16 +0100

    done stuff

--- src/pages/admin/banks/prestations.js
+++ src/pages/admin/banks/prestations.js
@@ -1,10 +1,66 @@
+
+import { useState } from "react";
 import AdminLayout from "@/layouts/adminLayout";
 import { getSession } from "next-auth/react";
-
+import LignePrestation from "@/components/admin/banks/conditionTarifaire/lignePrestation";
 function Prestations() {
+
+  const [prestations,setPrestations]=useState(
+    [
+        {
+            "nom_prestation": "Ouverture de compte et délivrance chéquier" ,
+            "categorie": "Ouverture Compte" ,
+            "tarif": 0,
+            "period": 0
+        } ,
+        {
+            "nom_prestation": "Frais de tenue de compte courant" ,
+            "categorie": "Tenue Compte" ,
+            "tarif": 2500,
+            "period": 90
+        } ,
+        {
+            "nom_prestation": "Frais de tenue de compte chèque" ,
+            "categorie": "Tenue Compte" ,
+            "tarif": 1000,
+            "period": 360
+        } ,
+        {
+            "nom_prestation": "Frais de tenue de compte sur livret" ,
+            "categorie": "Tenue Compte" ,
+            "tarif": 0,
+            "period": 0
+        } ,
+        {
+            "nom_prestation": "Fermeture compte courant" ,
+            "categorie": "Tenue Compte" ,
+            "tarif": 0,
+            "period": 0
+        },
+        {
+            "nom_prestation": "Fermeture compte chèque" ,
+            "categorie": "Fermeture Compte" ,
+            "tarif": 0,
+            "period": 0
+        },
+        {
+            "nom_prestation": "Fermeture compte sur livret" ,
+            "categorie": "Fermeture Compte" ,
+            "tarif": 0,
+            "period": 0
+        },
+        {
+            "nom_prestation": "Fermeture compte devise" ,
+            "categorie": "Fermeture Compte" ,
+            "tarif": 0,
+            "period": 0
+        }
+    ]
+  )
+
   return(
     <div>
-      what's up
+      <LignePrestation prestation1={prestations[0]} prestation2={prestations[1]}/>
     </div>
   );
 }

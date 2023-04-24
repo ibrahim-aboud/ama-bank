9fc170148c1eb947645d1257738a3dbb3ecffe46
Raouf Medjadj <lm_medjadj@esi.dz>
2023-04-24 14:41:32 +0100

    added custom name for prestation

--- src/components/admin/add-prestation-popup.js
+++ src/components/admin/add-prestation-popup.js
@@ -1,5 +1,4 @@
-
-import { MdOutlineClose } from "react-icons/md";
+import { MdOutlineAddBox } from "react-icons/md";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -7,7 +6,8 @@ export default function AddPrestPopup({ isVisible, setIsVisible, bankId, prestat
 
     const [prestation, setPrestation] = useState();
     const [categories, setCategories] = useState([]);
-    const [prestations, setPrestations] = useState();
+    const [prestations, setPrestations] = useState([]);
+    const [personalisedPrest, setPersonalisedPrest] = useState(false);
 
     useEffect(() => {
         setPrestation(
@@ -32,56 +32,77 @@ export default function AddPrestPopup({ isVisible, setIsVisible, bankId, prestat
 
     async function onAdd() {
         
-        const prestationToSend = {...prestation, bank_id: bankId};
+        var prestationToSend = {...prestation, bank_id: bankId};
+        prestationToSend = {...prestationToSend, categorie_operation: categories.find(item => item.id === prestation.categorie_id).name}; 
 
         //await axios.post(process.env.NEXT_PUBLIC_API_URL + "/prestations", {prestation: prestationToSend});
+        setPrestation(prestationToSend);
+        console.log(prestationToSend);
 
         setIsVisible(!isVisible);
+        setPersonalisedPrest(!personalisedPrest);
     }
-
 
     if (!isVisible) return null;
     return (
         <div>
             <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center ">
                 <form onSubmit={onAdd} className="bg-white rounded flex flex-col items-center w-[700px]">
-                    <div className="bg-green-100 w-full flex justify-center py-4 rounded-t mb-4">
-                        <h2 className="text-lg font-medium">Ajouter une nouvelle prestation</h2>
+                    <div className="bg-[#40916de3] text-white w-full flex justify-center items-center py-4 rounded-t mb-5">
+                        <MdOutlineAddBox size={25} />
+                        <h2 className="text-lg font-medium ml-2">Ajouter une nouvelle prestation</h2>
                     </div>
                     <div className="flex flex-col">
                         <select
                             id="categorie"
                             name="categorie"
                             required
-                            className="rounded bg-gray-100 outline-none border w-[350px] pl-3 py-2"
-                            onChange={(event) => {setPrestation({...prestation, categorie_id: parseInt(event.target.value)}); setPrestation({...prestation, categorie_operation: categories[parseInt(event.target.value)]})}}
+                            className="rounded bg-gray-100 outline-none border w-[450px] pl-3 py-2"
+                            onChange={(event) => {setPrestation({...prestation, categorie_id: parseInt(event.target.value)})}}
                         >
                             <option value="">--- Catégorie prestation ---</option>
-                            {categories.map((item, index) => (
-                                <option value={index}>{item.name}</option> // in case you forget, here you need to get the data from the db first, so replace this later
+                            {categories.map((item) => (
+                                <option key={item.id} value={item.id}>{item.name}</option>
                             ))}
                         </select>
+
+                        {personalisedPrest && (
+                            <input 
+                                id="nom_prest_custom"
+                                name="nom_prest_custom"
+                                type="text"
+                                required
+                                placeholder="Nouvelle prestation"
+                                className={`rounded bg-gray-100 outline-none border w-[450px] px-5 py-2 mt-3 ${personalisedPrest ? "" : "hidden"}`}
+                                onChange={(event) => {setPrestation({...prestation, name: event.target.value})}}
+                            />
+                        )}
 
                         <select
                             id="nom_prestation"
                             name="nom_prestation"
                             required
-                            className="rounded bg-gray-100 outline-none border w-[350px] pl-3 py-2 mt-2"
-                            onChange={(event) => {setPrestation({...prestation, name: event.target.value})}} 
+                            className={`rounded bg-gray-100 outline-none border w-[450px] pl-3 py-2 mt-3 ${personalisedPrest ? "hidden" : ""}`}
+                            onChange={(event) => {
+                                if (event.target.value == "#NEW_CUSTOM") setPersonalisedPrest(!personalisedPrest);
+                                setPrestation({...prestation, name: event.target.value});
+                            }} 
                         >
                             <option value="">--- Nom prestation ---</option>
+                            <option value="#NEW_CUSTOM">Nouvelle prestation personnalisée</option>
+
                             <option value="Ouverture compte">Ouverture compte</option>  {/* in case you forget, here you need to get the data from the db first, so replace this later */}
                             <option value="Fermeture compte">Fermeture compte</option>
                             <option value="Autre prestation">Autre prestation</option>
                             <option value="Autre prestation 2">Autre prestation 2</option>
                         </select>
                         
-                        <div className="flex justify-between my-2">
+                        <div className="flex justify-between my-3">
                             <select
                                 id="type_prestation"
                                 name="type_prestation"
                                 required
-                                className="rounded bg-gray-100 outline-none border w-[170px] pl-3 py-2"
+                                className="rounded bg-gray-100 outline-none border w-[220px] pl-3 py-2"
                                 onChange={(event) => {setPrestation({...prestation, type: event.target.value})}}
                             >
                                 <option value="">--- Type ---</option>
@@ -94,7 +115,7 @@ export default function AddPrestPopup({ isVisible, setIsVisible, bankId, prestat
                                 id="nom_prestation"
                                 required
                                 name="nom_prestation"
-                                className="rounded bg-gray-100 outline-none border w-[170px] pl-3 py-2"
+                                className="rounded bg-gray-100 outline-none border w-[215px] pl-3 py-2"
                                 onChange={(event) => {setPrestation({...prestation, period: parseInt(event.target.value)})}}
                             >
                                 <option value="">--- Périodicité ---</option>
@@ -115,13 +136,13 @@ export default function AddPrestPopup({ isVisible, setIsVisible, bankId, prestat
                             step="1"
                             required
                             placeholder="Tarif (gratuit par defaut)"
-                            className="rounded bg-gray-100 outline-none border pl-5 py-2"
+                            className="rounded bg-gray-100 outline-none border px-5 py-2"
                             onChange={(event) => {setPrestation({...prestation, tarif: parseInt(event.target.value)})}}
                         />
                     </div>
-                    <div className="flex mb-5 mt-4">
+                    <div className="flex mb-5 mt-5">
                         <button className="p-2 rounded border hover:bg-[#40916d9a] hover:ease-in-out duration-100">Ajouter</button>
-                        <button className="p-2 ml-4 rounded border hover:bg-red-100 hover:ease-in-out duration-100" onClick={() => {setIsVisible(!isVisible)}}>Annuler</button>
+                        <button className="p-2 ml-4 rounded border hover:bg-red-100 hover:ease-in-out duration-100" onClick={() => {setIsVisible(!isVisible); setPersonalisedPrest(!personalisedPrest)}}>Annuler</button>
                     </div>
                 </form>
             </div>

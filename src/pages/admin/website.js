a3868e9596908b1bc250e30c2fd1eef2442aeed4
Mounir <lm_melzi@esi.dz>
2023-04-24 17:20:27 +0200

    add confirm popup to the form

--- src/pages/admin/website.js
+++ src/pages/admin/website.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import AdminLayout from "@/layouts/adminLayout";
+import ConfirmPopup from "@/components/admin/confirmPopup";
 import { getSession } from "next-auth/react";
 import { FiUpload, FiPhoneCall } from "react-icons/fi";
 import { MdCancel, MdEdit, MdFax, MdEmail } from "react-icons/md";
@@ -18,6 +19,7 @@ import {
 function Website({ infos }) {
   const [websiteInfos, setWebsiteInfos] = useState(infos);
   const [loading, setLoading] = useState(false);
+  const [showConfirmPopup, setShowConfirmPopup] = useState(false);
   const [error, setError] = useState("");
   const [selectedLogo, setSelectedLogo] = useState("");
   const [selectedLogoFile, setSelectedLogoFile] = useState(null);
@@ -79,7 +81,12 @@ function Website({ infos }) {
         <div className="hidden lg:block h-[2px] bg-black w-[25%]" />
       </div>
 
-      <form className="flex flex-col items-center" onSubmit={sumbitHandler}>
+      <form
+        className="flex flex-col items-center"
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
         {/* logo */}
         <div className="mx-5 mb-4 w-[90%] lg:w-[900px]">
           <label htmlFor="logo" className="block p-1 ">
@@ -334,13 +341,24 @@ function Website({ infos }) {
 
         <div className="mt-4 flex flex-col md:flex-row items-center justify-center gap-5 md:gap-10 w-full lg:w-[800px] lg:justify-between">
           <button
-            type="submit"
             disabled={loading}
             className="mb-1 rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-green-600 disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
+            onClick={(event) => {
+              event.preventDefault();
+              setShowConfirmPopup(true);
+            }}
           >
             Sauvegarder les modifications
             <HiCheckCircle size={23} className="ml-2" />
           </button>
+
+          {showConfirmPopup && (
+            <ConfirmPopup
+              message={"Voulez-vous confirmer les modifications ?"}
+              onConfirm={(e) => sumbitHandler(e)}
+              onExit={() => setShowConfirmPopup(false)}
+            />
+          )}
 
           <button
             type="reset"

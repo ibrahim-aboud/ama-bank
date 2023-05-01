de863f3d3bc512aa8217920cf0d21d33fd0a72cd
F1OOw <ls_djerrai@esi.dz>
2023-05-01 22:40:27 +0100

    pre commit

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,12 +2,16 @@ import React from "react"
 import Image from "next/image"
 import styles from "@/styles/Filter.module.css"
 import { useRef } from "react";
+import { useState, useEffect } from "react";
 
-const Filter = ({types_comptes}) => {
-    const fltrs = ["Gestion et tenue de compte", "Opérations de paiement", "Monétique"] ;
+const Filter = ({types_comptes, types_prestations, prestations, setPrestations}) => {
     const arrow = useRef(null) ;
     const checkboxes = useRef(null);
-    
+    const selectBar = useRef(null) ;
+    const checkForm = useRef(null) ;
+    const [common, setCommon] = useState(null) ;
+    // const [localPrestations, setLocalPrestations] = useState([]) ;
+
     const handleClick = ()=>{
         if (checkboxes.current.style.display != 'none'){
             checkboxes.current.style.display = "none" ;
@@ -18,8 +22,47 @@ const Filter = ({types_comptes}) => {
         }
     }
 
-    const submitForm = ()=>{
+    const handleSelect = ()=>{
+        var val = selectBar.current.value ;
+        var localPrestations =  prestations.filter((prst)=>{
+            return prst.type.toUpperCase() == val.toUpperCase() ;
+        }) ;
 
+        checkForm.current.childNodes.forEach(node=>{
+            node.childNodes[0].checked = false ;
+        })
+
+        setPrestations(localPrestations) ;
+        setCommon(localPrestations) ;
+    }
+
+    useEffect(()=>{
+        handleSelect() ;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [prestations]) ;
+
+    const handleChange = ()=>{
+        var finals = [] ;
+        var checks  = checkForm.current.childNodes ;
+        var num = 0 ;
+
+        checks.forEach(element => {
+            if (element.childNodes[0].checked){
+                var newPrestations = common.filter(prst=>{
+                    return prst.categorie_operation == element.childNodes[0].value
+                })
+                finals = [...finals,...newPrestations] ; 
+                num=num+1 ;
+            } else {
+                num=num-1 ;
+            }
+        });
+        
+        if (num>-2){
+            setPrestations(finals) ;
+        } else {
+            setPrestations(common) ;
+        }
     }
 
   return (
@@ -27,11 +70,11 @@ const Filter = ({types_comptes}) => {
             <div className={styles.filters}>
                 <div className={styles.type_compte_container}>
                     <label className={styles.type_compte_label}>Type de compte</label>
-                    <select name={styles.types_comptes_selectbar} id="types_prestations_selectbar" className={styles.types_comptes_selectbar}>
+                    <select name={styles.types_comptes_selectbar} ref={selectBar} className={styles.types_comptes_selectbar} onChange={handleSelect}>
                     {
                         types_comptes.map((type,index)=>(
-                            <option value={type} key={index}>
-                                {type}
+                            <option value={type} key={index} selected>
+                                {type[0].charAt(0).toUpperCase() + type.slice(1)}
                             </option>
                         ))
                     }
@@ -41,27 +84,28 @@ const Filter = ({types_comptes}) => {
                     <label className={styles.types_prestations_label}>
                         Type Prestations
                     </label>
-                    <div className={styles.T} onClick={handleClick}>
-                        <Image src="/Vectors/ic_round-keyboard-arrow-down.svg" ref={arrow} width={30} height={30} />
+                    <div className={styles.T} onClick={handleClick} onChange={(event)=>{handleChange(event)}} >
+                        <Image src="/assets/icons/icon1.svg" alt="arrow" ref={arrow} width={30} height={30} />
                     </div>
                 </div>
             </div>
-            <div className={styles.checkBoxes} ref={checkboxes}>
-                <form action="">
+            <div className={styles.checkBoxes} style={{display:"none"}} ref={checkboxes}>
+                <form action="" ref={checkForm} onChange={handleChange}>
                     {
-                        fltrs.map((val,index)=>(
-                            <div key={index} className={styles.check}>
-                                <input type="checkbox" id={val} value={val}/>
-                                <label htmlFor={val}>{val}</label>
+                        types_prestations.map((val,index)=>(
+                            <div key={index} className={styles.check} value={val}>
+                                <input type="checkbox" id={index} value={val}/>
+                                <label htmlFor={index}>{val}</label>
                             </div>
                         ))
                     }
                 </form>
             </div>
+
             <div className={styles.start_filter}>
-                <button className={styles.start_compare_btn} onClick={submitForm}  >
+                <button className={styles.start_compare_btn}>
                 <span>Filter les champs de la comparaison</span>
-                <Image src="/Vectors/params_vector.svg" width={50} height={50} className={styles.param} />
+                <Image src="/Vectors/params_vector.svg" width={50} height={50} alt="param" className={styles.param} />
                 </button>
             </div>
         </div>
--- src/pages/compare.js
+++ src/pages/compare.js
@@ -1,18 +1,44 @@
 import Filter from '../components/Filter';
+import { useState } from 'react';
 
-export const getServerSideProps = () =>{
-    const a = ['Particuliers','Professionels','Entreprise']
-    return {
-        props : {types_comptes: a}
-    }
-}
+const Compare = ({types_comptes,types_prestations}) => {
+    const [conditions,setConditions] = useState([]) ;
 
-const compare = ({types_comptes}) => {
+    const [FilteredConditions,setFilteredConditions] = useState([]) ;
+
+    
+
     return (
-      <div> 
-        <Filter types_comptes={types_comptes} />
-      </div> 
+      <div>
+        <Filter types_comptes={types_comptes} types_prestations={types_prestations} prestations={conditions} setPrestations={setFilteredConditions}></Filter>
+      </div>
     );
-  };
+};
   
-export default compare;
+export default Compare;
+
+export async function getServerSideProps(context) {
+  var props = {banks:[],types_comptes:[],types_prestations:[]} ;
+
+  try {
+    var response = await axios.get(
+      process.env.NEXT_PUBLIC_API_URL + `/prestations/types`
+    ) ;
+    props.types_comptes = response.data.types ;
+
+    response = await axios.get(
+      process.env.NEXT_PUBLIC_API_URL + `/prestations/categories`
+    ) ;
+    props.types_prestations = response.data.categories ;
+
+    response = await axios.get(
+      process.env.NEXT_PUBLIC_API_URL + `/banks`
+    ) ;
+    props.banks = response.data.banks ;
+      
+  } catch(err){
+    
+  }
+  
+  return {props}
+}

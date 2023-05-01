fd4fe589e433fea280d9ee306eb8af4be8e2515f
Oualid CHABANE <chbwalid7@gmail.com>
2023-05-01 22:50:39 +0200

    finalfinal

--- src/components/common/editBankDataPageClient/agencyListe.js
+++ src/components/common/editBankDataPageClient/agencyListe.js
@@ -24,12 +24,12 @@ function agencyListe({handleDeleteAgency, handleEditAgencyInfo, agencyList}){
                 handleEditAgencyInfo={handleEditAgencyInfo} />
     })
 
-    if (agencyList.length > 0) {
-      editErr({ display: "none" });
-      if (agencyList[0].id == -1) {
+    if (agencyList.length > 0 && agencyList[0].id == -1) {
+   
         border = agencyList.length > 1;
         agenciesList[0] = (
-          <AgencyRow
+          <AgencyRow 
+            key = {agencyList[0].id}
             record={agencyList[0]}
             isDg={"Siège sociale : "}
             agencyIcone={dgIcone}
@@ -37,22 +37,21 @@ function agencyListe({handleDeleteAgency, handleEditAgencyInfo, agencyList}){
           />
         );
       }
-    } else {
-      editErr({
-        display: "flex",
-        gap: "10px",
-        justifyContent: "center",
-        alignItems: "center",
-        fontFamily: `"Nunito", sans-serif`,
-        fontSize: "25px",
-      });
-    }
+  
 
 
     return(
         <div>
              {agenciesList}
-             <div style={errStyle}>
+             <div style={agencyList.length == 0 ?
+             {
+              display: "flex",
+              gap: "10px",
+              justifyContent: "center",
+              alignItems: "center",
+              fontFamily: `"Nunito", sans-serif`,
+              fontSize: "25px",
+            } : { display: "none" }}>
                 <Image src={emptyBox} alt="icon" style={{ height: '2em', width: 'auto' }} />
                 <span >Liste vide!</span>
              </div>

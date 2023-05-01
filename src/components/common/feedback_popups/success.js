f45e8bb36fc985506029aaf44276a6abceccddaf
Raouf Medjadj <lm_medjadj@esi.dz>
2023-05-01 16:03:40 +0100

    incremented z of feedback popups

--- src/components/common/feedback_popups/success.js
+++ src/components/common/feedback_popups/success.js
@@ -4,7 +4,7 @@ export default function SuccessFeedback({message, isVisible, isSuccessful}) {
     
     return (
         <div>
-            <div className={`fixed bottom-0 left-0 right-0 bg-opacity-0 flex items-center justify-center z-50 transform transition duration-150 ease-in-out ${isVisible && isSuccessful ? "translate-y-[-20px]" : "translate-y-full"}`}>
+            <div className={`fixed bottom-0 left-0 right-0 bg-opacity-0 flex items-center justify-center z-[60] transform transition duration-150 ease-in-out ${isVisible && isSuccessful ? "translate-y-[-20px]" : "translate-y-full"}`}>
                 <div className="">
                     <div className='flex drop-shadow-lg items-center font-semibold bg-[#79c428e0] text-white rounded-t-md p-2 sm:p-4'>
                         <BsCheckCircleFill size={18} />

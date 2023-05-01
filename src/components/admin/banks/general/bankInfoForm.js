624e281151ffa222a68b016dbbb3a4d67383a3b7
Mounir <lm_melzi@esi.dz>
2023-05-01 17:01:57 +0200

    add popups to edit bank and change green color

--- src/components/admin/banks/general/bankInfoForm.js
+++ src/components/admin/banks/general/bankInfoForm.js
@@ -7,11 +7,15 @@ import { FiGlobe, FiUpload } from "react-icons/fi";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import { MdCancel } from "react-icons/md";
 import { HiCheckCircle } from "react-icons/hi";
+import SuccessFeedback from "@/components/common/feedback_popups/success";
+import FailFeedback from "@/components/common/feedback_popups/fail";
+import bankInfoValidator from "@/lib/validations/bankInfoValidator";
 
 function BankInfoForm({ bankId }) {
   const [bank, setBank] = useState(null);
   const [oldInfo, setOldInfo] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
   const [showConfirmPopup, setShowConfirmPopup] = useState(false);
   const [error, setError] = useState("");
   const [selectedLogo, setSelectedLogo] = useState("");
@@ -128,8 +132,14 @@ function BankInfoForm({ bankId }) {
       });
 
       // when the data is updated
+      setShowConfirmPopup(false);
+
       setError("");
-      router.reload();
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+        // router.reload()
+      }, 3000);
     } catch (e) {
       setError(e.response?.data.error.message);
     }
@@ -160,7 +170,7 @@ function BankInfoForm({ bankId }) {
           <label htmlFor="bank_name" className="block p-1">
             Nom de la banque
           </label>
-          <div className="bg-gray-100 p-4 rounded-md border flex items-center w-full">
+          <div className="bg-gray-100 p-4 rounded-md border flex items-center w-full focus-within:border-green-800">
             <input
               type="text"
               name="bank_name"
@@ -180,7 +190,7 @@ function BankInfoForm({ bankId }) {
             <label htmlFor="bank_logo" className="block p-1 ">
               Logo de la banque
             </label>
-            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
+            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center focus-within:border-green-800">
               <input
                 type="file"
                 name="bank_logo"
@@ -222,7 +232,7 @@ function BankInfoForm({ bankId }) {
             <label htmlFor="bank_image" className="block p-1 ">
               Image de la banque
             </label>
-            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
+            <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center focus-within:border-green-800">
               <input
                 type="file"
                 name="bank_image"
@@ -265,7 +275,7 @@ function BankInfoForm({ bankId }) {
           <label htmlFor="description" className="block p-1">
             Description de la banque
           </label>
-          <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
+          <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center focus-within:border-green-800">
             <textarea
               id="description"
               name="bank_description"
@@ -284,7 +294,7 @@ function BankInfoForm({ bankId }) {
           <label htmlFor="bank_url" className="block p-1">
             Lien du site Web
           </label>
-          <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center">
+          <div className="bg-gray-100 w-full p-4 rounded-md border flex items-center focus-within:border-green-800">
             <FiGlobe className="text-gray-600" size={24} />
             <input
               type="text"
@@ -300,19 +310,31 @@ function BankInfoForm({ bankId }) {
           </div>
         </div>
 
-        {error && (
-          <div className="text-rose-500 font-bold text-center overflow-hidden mt-2">
-            {error}
-          </div>
-        )}
+        <FailFeedback message={error} isVisible={error} isSuccessful={!error} />
+        <SuccessFeedback
+          message={"Les information sont mise à jour avec succès"}
+          isVisible={success}
+          isSuccessful={success}
+        />
 
         <div className="mt-4 flex flex-col md:flex-row items-center justify-center gap-5 md:gap-10 w-full lg:w-[800px] lg:justify-between">
           <button
             disabled={loading}
-            className="mb-1 rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-green-600 disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
+            className="mb-1 rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-[#40916C] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
             onClick={(event) => {
               event.preventDefault();
-              setShowConfirmPopup(true);
+
+              const check = bankInfoValidator({
+                ...bank,
+                updateDate: bank.updateDate.substring(0, 10),
+              });
+
+              if (check.error) {
+                setError(check.errorList[0]);
+                setTimeout(() => {
+                  setError("");
+                }, 3000);
+              } else setShowConfirmPopup(true);
             }}
           >
             Sauvegarder les modifications
@@ -321,9 +343,15 @@ function BankInfoForm({ bankId }) {
 
           {showConfirmPopup && (
             <ConfirmPopup
+              title="Sauvegarder les modifications"
+              icon={<HiCheckCircle size={20} className="mr-3" />}
+              color="#40916C"
               message={"Voulez-vous confirmer les modifications ?"}
               onConfirm={(e) => sumbitHandler(e)}
-              onExit={() => setShowConfirmPopup(false)}
+              onExit={() => {
+                setError("");
+                setShowConfirmPopup(false);
+              }}
             />
           )}
 
@@ -342,7 +370,7 @@ function BankInfoForm({ bankId }) {
               setSelectedImage("");
               imageInputRef.current.value = "";
             }}
-            className="rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-green-600 disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
+            className="rounded-xl px-8 py-3 font-semibold bg-black text-white shadow-xl hover:bg-[#40916C] disabled:bg-slate-900 flex items-center hover:ease-in-out duration-300"
           >
             Annuler les modifications
             <MdCancel size={23} className="ml-2" />

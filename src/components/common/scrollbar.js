dd74cb905eccfdf292ff99d8a2abead34984e895
Mounir <lm_melzi@esi.dz>
2023-05-08 23:04:12 +0200

    fix scrollbar issue

--- src/components/common/scrollbar.js
+++ src/components/common/scrollbar.js
@@ -1,11 +1,11 @@
 import PerfectScrollbar from "react-perfect-scrollbar";
 import "react-perfect-scrollbar/dist/css/styles.css";
 
-function Scrollbar({ children }) {
+function Scrollbar({ children, wheelPropagation=true }) {
   return (
     <PerfectScrollbar
       options={{
-        wheelPropagation: false,
+        wheelPropagation: wheelPropagation,
         swipeEasing: true,
         wheelSpeed: 0.75,
       }}
